35cc7c514fdaa40a9348ca41b0cfa1f3081fc359
David Choque Llanos <davicho.llanos@gmail.com>
2021-02-03 19:30:40 +0100

    corregido error en setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='loggers,
+    name='loggers',
     version='0.0.1',
     description='coleccion de logs usados en diferentes proyectos',
     url='git@https://github.com/davi-cho/logging_python.git',
